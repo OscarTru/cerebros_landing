5beaced23678570606d946ec3689fca016fcf6a7
Oscar Trujillo <trujilloreyesoscarmi@gmail.com>
2026-04-10 17:09:07 +0200

    feat(blog): register /blog/:slug route

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import "./index.css"
 import App from "./App.tsx"
+import { BlogPost } from "./pages/BlogPost.tsx"
 import { MediaKit } from "./pages/MediaKit.tsx"
 import { Terminos } from "./pages/Terminos.tsx"
 import { Privacidad } from "./pages/Privacidad.tsx"
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
         <Route path="/media-kit" element={<MediaKit />} />
         <Route path="/terminos" element={<Terminos />} />
         <Route path="/privacidad" element={<Privacidad />} />
